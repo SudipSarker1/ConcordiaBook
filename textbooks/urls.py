a201faaac2c3254d224e8bf1c131af572e8fa32c
SudipSarker1 <su_sarke@live.concordia.ca>
2025-03-03 18:10:34 -0500

    views.py updated, url routing setuped

---
+++ textbooks/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('<str:course_code>/', views.textbook_list, name='textbook_list'),
+]
--- textbooks/views.py
+++ textbooks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Textbook
 
-# Create your views here.
+def textbook_list(request, course_code):
+    textbooks = Textbook.objects.filter(course_code=course_code, availability=True)
+    context = {
+        'course_code': course_code,
+        'textbooks': textbooks,
+    }
+    return render(request, 'textbooks/textbook_list.html', context)
