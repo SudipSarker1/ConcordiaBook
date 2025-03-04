17e42081c73637647b8ac42019208b47ea5219d2
SudipSarker1 <su_sarke@live.concordia.ca>
2025-03-03 20:59:13 -0500

    image field added

--- concordiabook/settings.py
+++ concordiabook/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -70,7 +69,6 @@ WSGI_APPLICATION = 'concordiabook.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -80,7 +78,6 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -98,21 +95,19 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# MEDIA SETTINGS
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- textbooks/models.py
+++ textbooks/models.py
@@ -7,6 +7,7 @@ class Textbook(models.Model):
     course_code = models.CharField(max_length=20)
     condition = models.CharField(max_length=50, blank=True, null=True)
     availability = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='textbook_images/', blank=True, null=True)
 
     def __str__(self):
         return f"{self.title} ({self.course_code})"
